668626b387f4e8b90bf64ba5c582dc901e53fff3
msDos <mathieumsdos@gmail.com>
2025-01-03 14:08:21 +0100

    start reponsive navBar burger menu

--- src/components/welcomePageNavBar/WelcomePageNavBar.tsx
+++ src/components/welcomePageNavBar/WelcomePageNavBar.tsx
@@ -14,24 +14,34 @@ const WelcomePageNavBar = () => {
   return (
     <header className="flex justify-center items-center">
       <nav className="flex w-full  justify-between items-center">
-     
-            <Link href="/">
-              <Image src="/logo/Logo_Retwitter.png" width={170} height={50} alt="logo" />
+        <Link href="/">
+          <Image
+            src="/logo/Logo_Retwitter.png"
+            width={170}
+            height={50}
+            alt="logo"
+          />
+        </Link>
+        <ul className="hidden md:flex gap-5  ">
+          {navItems.map((item) => (
+            <Link key={item.name} href={item.href}>
+              <li
+                className="hover:text-purpleLight transition-all duration-500"
+                key={item.name}
+              >
+                {item.name}
+              </li>
             </Link>
-          <ul className="flex gap-5 ">
-            {navItems.map((item) => (
-              <Link key={item.name} href={item.href}>
-                <li className="hover:text-purpleLight transition-all duration-500" key={item.name}>{item.name}</li>
-              </Link>
-            ))}
-          </ul>
-
-        <div>
+          ))}
+        </ul>
+        <div className="hidden md:flex">
           <PrimaryButton
             text="Login"
             onClick={async () => await signIn("google")}
           />
         </div>
+        {/* burger menu */}
+        
       </nav>
     </header>
   );
