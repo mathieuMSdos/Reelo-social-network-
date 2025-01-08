49070c8be0ae2a61450fbfe868bcd79ed78ac27a
msDos <mathieumsdos@gmail.com>
2025-01-08 21:16:51 +0100

    design logout button DONE

--- src/components/design/secondaryButtonInApp/SecondaryButtonInApp.tsx
+++ src/components/design/secondaryButtonInApp/SecondaryButtonInApp.tsx
@@ -3,9 +3,9 @@ import styles from "./secondaryButtonInApp.module.css";
 const SecondaryButtonInApp = () => {
   return (
     <button
-      className={`${styles.button_shadow} h-8 w-20 flex justify-center items-center px-2 py-2 rounded-lg text-sm whitespace-nowrap font-inter font-semibold `}
+      className={`${styles.button_shadow} h-8 w-20 flex justify-center items-center px-2 py-2 rounded-lg text-sm whitespace-nowrap font-inter font-semibold overflow-hidden`}
     >
-      Sign out
+      <p className={`${styles.text_inner_shadow}`}>Sign out</p>
     </button>
   );
 };
