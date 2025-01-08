22954347a575fed5cefb2f5c782be80d93cc0dba
msDos <mathieumsdos@gmail.com>
2025-01-08 22:17:44 +0100

    signout button DONE

--- src/components/design/secondaryButtonInApp/SecondaryButtonInApp.tsx
+++ src/components/design/secondaryButtonInApp/SecondaryButtonInApp.tsx
@@ -1,11 +1,13 @@
+import { LogOut } from "lucide-react";
 import styles from "./secondaryButtonInApp.module.css";
 
 const SecondaryButtonInApp = () => {
   return (
     <button
-      className={`${styles.button_shadow} h-8 w-20 flex justify-center items-center px-2 py-2 rounded-lg text-sm whitespace-nowrap font-inter font-semibold overflow-hidden`}
+      className={`${styles.button_shadow} h-8 w- flex gap-1 justify-center items-center px-2 py-2 rounded-lg text-sm whitespace-nowrap font-inter font-semibold overflow-hidden`}
     >
-      <p className={`${styles.text_inner_shadow}`}>Sign out</p>
+      <LogOut size={16} strokeWidth={2.5} color="#8952E0" />
+      <p className={`${styles.text_inner_shadow} text-purpleBtn`}>Sign out</p>
     </button>
   );
 };
