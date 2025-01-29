df0780e9b1a8f165ab3717d8015f0da5c6e597c3
msDos <mathieumsdos@gmail.com>
2025-01-29 20:12:00 +0100

    like motion design DONE

--- src/components/CRUD components/feedPostColumn/postProfileItem/postLikeButton/PostLikeButton.tsx
+++ src/components/CRUD components/feedPostColumn/postProfileItem/postLikeButton/PostLikeButton.tsx
@@ -4,7 +4,9 @@ import { likePostAction } from "@/app/actions/socialActions/likePost/likePost.ac
 import { unlikePostAction } from "@/app/actions/socialActions/likePost/unLikePost.action";
 import { useStore } from "@/lib/store/index.store";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { motion } from "framer-motion";
 import { Heart } from "lucide-react";
+import { useState } from "react";
 
 interface PostLikeButtonProps {
   authorId: string;
@@ -21,6 +23,8 @@ const PostLikeButton = ({
   likedBy,
   userAlreadyLikeThisPost,
 }: PostLikeButtonProps) => {
+  const [isAnimating, setIsAnimating] = useState(false);
+
   const userId = useStore((state) => state.userId);
   const queryClient = useQueryClient();
 
@@ -131,27 +135,52 @@ const PostLikeButton = ({
     },
   });
 
+  const handleClickLikeButton = () => {
+    if (authorId === userId) {
+      // si userid est l'auteur du post on l'empêche de liker (en front ici mais dans le server action il y a une protection aussi)
+      return;
+    }
+    // modifier le state pour lancer l'animation
+    if (!isAnimating) {
+      setIsAnimating(true);
+    }
+
+    if (!userAlreadyLikeThisPost) {
+      // si l'utilisateur n'a  jamais liké le post on joue la mutation like
+      likeMutation();
+    } else {
+      // sinon c'est la fonction unlike on joue la mutation unlike
+      unlikeMutation();
+    }
+  };
+
   return (
     <button
       className="flex gap-1 items-center justify-center"
-      onClick={() =>
-        // si l'utilisateur n'a  jamais liké le post on lui présente la fonction pour liker
-        {
-          if (!userAlreadyLikeThisPost) {
-            likeMutation();
-          } else {
-            unlikeMutation();
-          }
-        }
-      }
+      onClick={() => handleClickLikeButton()}
     >
-      <Heart
-        className={`${
-          userAlreadyLikeThisPost ? "text-purpleBtn" : "text-textGrey"
-        }  transition-all duration-100 `}
-        fill="currentColor"
-        size={20}
-      />
+      <motion.div
+        animate={
+          isAnimating
+            ? {
+                scale: [1, 0.5, 1.5, 1],
+                transition: {
+                  duration: 0.3,
+                  onComplete: () => setIsAnimating(false),
+                },
+              }
+            : {}
+        }
+      >
+        <Heart
+          className={`${
+            userAlreadyLikeThisPost ? "text-purpleBtn" : "text-textGrey"
+          }  transition-all duration-100 `}
+          fill="currentColor"
+          size={20}
+        />
+      </motion.div>
+
       <p
         className={`${
           userAlreadyLikeThisPost ? "text-purpleBtn" : "text-textGrey"
